71bba7f8206c142f75911e24f470047fbf976148
Manthan Thakor <thakormanthan849@gmail.com>
2024-07-15 09:05:58 +0530

    Counter App 2

---
+++ script.js
@@ -0,0 +1,9 @@
+//! Make sure elements are loaded
+document.addEventListener("DOMContentLoaded", () => {
+  // select elements
+  const countervalue = document.getElementById("counter-value");
+  const decrementBtn = document.getElementById("decrement-btn");
+  const incrementBtn = document.getElementById("increment-btn");
+  const resetBtn = document.getElementById("reset-btn");
+  let counter = 0;
+});
