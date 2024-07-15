a6ee353de457cb2cd4a27693c9bc6e7fe3ae6a1e
Manthan Thakor <thakormanthan849@gmail.com>
2024-07-15 09:18:19 +0530

    Counter App 3

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 //! Make sure elements are loaded
 document.addEventListener("DOMContentLoaded", () => {
   // select elements
-  const countervalue = document.getElementById("counter-value");
-  const decrementBtn = document.getElementById("decrement-btn");
-  const incrementBtn = document.getElementById("increment-btn");
+  const counterValue = document.getElementById("counter-value");
+  const decrementBtn = document.getElementById("decrease-btn");
+  const incrementBtn = document.getElementById("increase-btn");
   const resetBtn = document.getElementById("reset-btn");
-  let counter = 0;
+  // global value
+  let count = 0;
+
+  // add events to the button
+
+  incrementBtn.addEventListener("click", () => {
+    count++;
+    updateCounter();
+  });
+  decrementBtn.addEventListener("click", () => {
+    count--;
+    updateCounter();
+  });
+  resetBtn.addEventListener("click", () => {
+    count = 0;
+    updateCounter();
+  });
+
+  // update counter function
+  function updateCounter() {
+    counterValue.textContent = count;
+  }
 });
